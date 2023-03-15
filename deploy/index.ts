d47b80b7a8c1b4aea0d952cd8a18a788ddbae842
AnshuJalan <anshujalan206@gmail.com>
2023-03-15 13:31:25 +0530

    feat: add deploy script

---
+++ deploy/index.ts
@@ -0,0 +1,39 @@
+import fs from "fs";
+import { MichelsonMap, TezosToolkit } from "@taquito/taquito";
+import { InMemorySigner } from "@taquito/signer";
+
+const tezos = new TezosToolkit("https://ghostnet.smartpy.io");
+
+tezos.setProvider({
+  signer: new InMemorySigner(process.env.PRIVATE_KEY as string),
+});
+
+(async () => {
+  try {
+    console.log("> Deploying segmented CFMM factory...");
+
+    // Load code
+    const code = fs.readFileSync(`${__dirname}/../src/michelson/factory.tz`).toString();
+
+    // Initial storage
+    const storage = {
+      admin: "tz1ZczbHu1iLWRa88n9CUiCKDGex5ticp19S",
+      proposed_admin: null,
+      pools: MichelsonMap.fromLiteral({}),
+      fee_tiers: MichelsonMap.fromLiteral({
+        1: 1,
+        10: 10,
+        30: 60,
+        100: 200,
+      }),
+    };
+
+    // Deploy
+    const op = await tezos.contract.originate({ code, storage });
+    await op.confirmation(1);
+
+    console.log(`>> Deployed at: ${op.contractAddress}`);
+  } catch (err) {
+    console.error(err);
+  }
+})();
