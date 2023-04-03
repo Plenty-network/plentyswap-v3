1c2f7d7358308d653948e91d76311e8a9d6bbad3
AnshuJalan <anshujalan206@gmail.com>
2023-04-03 15:35:15 +0530

    fix: fee tier

--- deploy/index.ts
+++ deploy/index.ts
@@ -22,7 +22,7 @@ tezos.setProvider({
       pools: MichelsonMap.fromLiteral({}),
       fee_tiers: MichelsonMap.fromLiteral({
         1: 1,
-        10: 10,
+        5: 10,
         30: 60,
         100: 200,
       }),
