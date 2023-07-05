7efeaad12b6a3c2dbf4c1f2d09b2416ac7b4d644
AnshuJalan <anshujalan206@gmail.com>
2023-07-05 14:48:22 +0530

    test: stake and unstake for farm

--- test/scenarios/farm/unstake.test.ts
+++ test/scenarios/farm/unstake.test.ts
@@ -1,5 +1,5 @@
 import { DefaultContractType, MichelsonMap, OpKind } from "@taquito/taquito";
-import { Math2, Stake, StakeManager, StakeOptions } from "@plenty-labs/v3-sdk";
+import { Math2, Stake, StakeManager, StakeOptions, UnstakeOptions } from "@plenty-labs/v3-sdk";
 
 import Tezos from "../../tezos";
 import { number } from "../../helpers/math";
@@ -8,7 +8,7 @@ import { accounts } from "../../helpers/accounts";
 import { DECIMALS, getDefaultFarmStorage } from "../../helpers/default";
 import { FA12Storage, Incentive, FarmStorage, Position } from "../../types";
 
-describe("farm.stake", () => {
+describe("farm.unstake", () => {
   let tezos: Tezos;
   let storage: FarmStorage;
   let token: DefaultContractType;
@@ -44,86 +44,7 @@ describe("farm.stake", () => {
     };
   });
 
-  it("correctly mints a fresh position", async () => {
-    // Arbitrary values for testing the farm logic
-    const position: Position = {
-      fee_growth_inside_last: {
-        x: number(0),
-        y: number(0),
-      },
-      liquidity: number(100),
-      lower_tick_index: number(-10),
-      upper_tick_index: number(10),
-      owner: accounts.alice.pkh,
-    };
-    const cumulativesSnapshot = {
-      tick_cumulative_inside: number(0),
-      seconds_per_liquidity_inside: number(50),
-      seconds_inside: number(0),
-    };
-    const incentive: Incentive = {
-      reward_token: { fa12: token.address },
-      start_time: NOW - 1000,
-      end_time: NOW + 1000,
-      claim_deadline: NOW + 2000,
-      total_reward: number(100 * DECIMALS),
-      total_reward_unclaimed: number(100 * DECIMALS),
-      total_seconds_claimed: number(0),
-      n_stakes: 0,
-      refundee: accounts.alice.pkh,
-    };
-
-    storage.incentives.set(1, incentive);
-
-    const core = await tezos.deployContract("dummyPool", {
-      position,
-      cumulatives_inside_snapshot: cumulativesSnapshot,
-    });
-
-    storage.cfmm_address = core.address;
-
-    const farm = await tezos.deployContract("farm", storage);
-
-    const options: StakeOptions = { incentiveId: 1, tokenId: 1 };
-
-    // When alice stakes a position
-    await tezos.sendBatchOp([{ kind: OpKind.TRANSACTION, ...StakeManager.stake(farm, options) }]);
-
-    // // block timestamp
-    // const timestamp = dateToTimestamp(
-    //   (await axios.get(`${config.rpcURL}/chains/main/blocks/${op.includedInBlock}`)).data.header
-    //     .timestamp
-    // );
-
-    const updatedStorage = await tezos.getStorage(farm);
-    const poolStorage = await tezos.getStorage(core.address);
-
-    const stake = await updatedStorage.stakes.get({ 0: 1, 1: 1 });
-    const deposit = await updatedStorage.deposits.get(1);
-    const updatedIncentive = await updatedStorage.incentives.get(1);
-
-    // the storage is updated correctly
-    expect(stake).toEqual({
-      seconds_per_liquidity_inside_last: cumulativesSnapshot.seconds_per_liquidity_inside,
-      liquidity: number(100),
-    });
-    expect(deposit).toEqual({
-      owner: accounts.alice.pkh,
-      n_stakes: number(1),
-      tick_range: { 2: number(-10), 3: number(10) },
-    });
-    expect(updatedIncentive.n_stakes).toEqual(number(1));
-
-    // Transfer is called correctly
-    expect(poolStorage.transfer_params).toEqual([
-      {
-        from_: accounts.alice.pkh,
-        txs: [{ to_: farm.address, token_id: number(1), amount: number(1) }],
-      },
-    ]);
-  });
-
-  it("correctly updates an existing stake and records the reward before incentive is over", async () => {
+  it("correctly unstakes and records the reward before incentive is over", async () => {
     // Arbitrary values for testing the farm logic
     const position: Position = {
       fee_growth_inside_last: {
@@ -169,6 +90,13 @@ describe("farm.stake", () => {
       n_stakes: 1,
       tick_range: { 2: -10, 3: 10 },
     });
+    storage.rewards.set(
+      {
+        0: { fa12: token.address },
+        1: accounts.alice.pkh,
+      },
+      number(2 * DECIMALS)
+    );
 
     const core = await tezos.deployContract("dummyPool", {
       position,
@@ -179,10 +107,10 @@ describe("farm.stake", () => {
 
     const farm = await tezos.deployContract("farm", storage);
 
-    const options: StakeOptions = { incentiveId: 1, tokenId: 1 };
+    const options: UnstakeOptions = { incentiveId: 1, tokenId: 1 };
 
-    // When alice stakes a position
-    await tezos.sendBatchOp([{ kind: OpKind.TRANSACTION, ...StakeManager.stake(farm, options) }]);
+    // When alice unstakes a position
+    await tezos.sendBatchOp([{ kind: OpKind.TRANSACTION, ...StakeManager.unstake(farm, options) }]);
 
     const updatedStorage = await tezos.getStorage(farm);
 
@@ -213,14 +141,114 @@ describe("farm.stake", () => {
       .multipliedBy(number(100));
 
     // the storage is updated correctly
-    expect(updatedStake).toEqual({
-      seconds_per_liquidity_inside_last: cumulativesSnapshot.seconds_per_liquidity_inside,
-      liquidity: number(150),
-    });
-    // Stays the same
+    expect(updatedStake).toEqual(undefined);
     expect(deposit).toEqual({
       owner: accounts.alice.pkh,
-      n_stakes: number(1),
+      n_stakes: number(0),
+      tick_range: { 2: number(-10), 3: number(10) },
+    });
+    expect(reward).toEqual(estimatedReward.plus(2 * DECIMALS)); // Existing reward gets added up
+    expect(updatedIncentive.total_reward_unclaimed).toEqual(
+      number(100 * DECIMALS).minus(estimatedReward)
+    );
+    expect(updatedIncentive.total_seconds_claimed).toEqual(estimatedSecondsClaimed);
+    expect(updatedIncentive.n_stakes).toEqual(number(0));
+  });
+
+  it("correctly unstakes and records the reward after incentive is over", async () => {
+    // Arbitrary values for testing the farm logic
+    const position: Position = {
+      fee_growth_inside_last: {
+        x: number(0),
+        y: number(0),
+      },
+      liquidity: number(150),
+      lower_tick_index: number(-10),
+      upper_tick_index: number(10),
+      owner: accounts.alice.pkh,
+    };
+    const cumulativesSnapshot = {
+      tick_cumulative_inside: number(0),
+      seconds_per_liquidity_inside: Math2.bitShift(number(50), -128)
+        .dividedBy(number(100))
+        .decimalPlaces(0),
+      seconds_inside: number(0),
+    };
+    const incentive: Incentive = {
+      reward_token: { fa12: token.address },
+      start_time: NOW - 1000,
+      end_time: NOW - 100, // Incentive is over in the past
+      claim_deadline: NOW + 2000,
+      total_reward: number(100 * DECIMALS),
+      total_reward_unclaimed: number(100 * DECIMALS),
+      total_seconds_claimed: number(0),
+      n_stakes: 1,
+      refundee: accounts.alice.pkh,
+    };
+
+    storage.incentives.set(1, incentive);
+    storage.stakes.set(
+      { 0: 1, 1: 1 },
+      {
+        seconds_per_liquidity_inside_last: Math2.bitShift(number(10), -128)
+          .dividedBy(number(100))
+          .decimalPlaces(0),
+        liquidity: number(100),
+      }
+    );
+    storage.deposits.set(1, {
+      owner: accounts.alice.pkh,
+      n_stakes: 1,
+      tick_range: { 2: -10, 3: 10 },
+    });
+
+    const core = await tezos.deployContract("dummyPool", {
+      position,
+      cumulatives_inside_snapshot: cumulativesSnapshot,
+    });
+
+    storage.cfmm_address = core.address;
+
+    const farm = await tezos.deployContract("farm", storage);
+
+    const options: UnstakeOptions = { incentiveId: 1, tokenId: 1 };
+
+    // When alice unstakes a position
+    await tezos.sendBatchOp([{ kind: OpKind.TRANSACTION, ...StakeManager.unstake(farm, options) }]);
+
+    const updatedStorage = await tezos.getStorage(farm);
+
+    const updatedStake = await updatedStorage.stakes.get({ 0: 1, 1: 1 });
+    const deposit = await updatedStorage.deposits.get(1);
+    const updatedIncentive = await updatedStorage.incentives.get(1);
+    const reward = await updatedStorage.rewards.get({
+      0: { fa12: token.address },
+      1: accounts.alice.pkh,
+    });
+
+    const stake = new Stake(
+      {
+        startTime: incentive.start_time,
+        endTime: incentive.end_time,
+        totalRewardUnclaimed: incentive.total_reward_unclaimed,
+        totalSecondsClaimed: incentive.total_seconds_claimed,
+      },
+      number(100),
+      Math2.bitShift(number(10), -128).dividedBy(number(100)).decimalPlaces(0)
+    );
+    const estimatedReward = stake.computeUnclaimedReward(
+      Math2.bitShift(number(50), -128).dividedBy(number(100)).decimalPlaces(0)
+    );
+
+    const estimatedSecondsClaimed = cumulativesSnapshot.seconds_per_liquidity_inside
+      .minus(Math2.bitShift(number(10), -128).dividedBy(number(100)).decimalPlaces(0))
+      .multipliedBy(number(100));
+
+    // the storage is updated correctly
+    expect(updatedStake).toEqual(undefined);
+    expect(deposit).toEqual({
+      owner: accounts.alice.pkh,
+      n_stakes: number(0),
       tick_range: { 2: number(-10), 3: number(10) },
     });
     expect(reward).toEqual(estimatedReward);
@@ -228,42 +256,53 @@ describe("farm.stake", () => {
       number(100 * DECIMALS).minus(estimatedReward)
     );
     expect(updatedIncentive.total_seconds_claimed).toEqual(estimatedSecondsClaimed);
+    expect(updatedIncentive.n_stakes).toEqual(number(0));
   });
 
-  it("fails if invalid incentive id is used", async () => {
+  it("fails for invalid incentive", async () => {
+    const incentive: Incentive = {
+      reward_token: { fa12: token.address },
+      start_time: NOW - 1000,
+      end_time: NOW - 100, // Incentive is over in the past
+      claim_deadline: NOW + 2000,
+      total_reward: number(100 * DECIMALS),
+      total_reward_unclaimed: number(100 * DECIMALS),
+      total_seconds_claimed: number(0),
+      n_stakes: 1,
+      refundee: accounts.alice.pkh,
+    };
+
+    storage.incentives.set(1, incentive);
+    storage.stakes.set(
+      { 0: 1, 1: 1 },
+      {
+        seconds_per_liquidity_inside_last: Math2.bitShift(number(10), -128)
+          .dividedBy(number(100))
+          .decimalPlaces(0),
+        liquidity: number(100),
+      }
+    );
+    storage.deposits.set(1, {
+      owner: accounts.alice.pkh,
+      n_stakes: 1,
+      tick_range: { 2: -10, 3: 10 },
+    });
+
     const farm = await tezos.deployContract("farm", storage);
 
-    const options: StakeOptions = { incentiveId: 1, tokenId: 1 }; // Invalid incentive
+    const options: UnstakeOptions = { incentiveId: 2, tokenId: 1 };
 
-    // When alice stakes for invalid incentive id, txn fails
+    // When alice unstakes a position for incentive id 2, txn fails
     await expect(
-      tezos.sendBatchOp([{ kind: OpKind.TRANSACTION, ...StakeManager.stake(farm, options) }])
+      tezos.sendBatchOp([{ kind: OpKind.TRANSACTION, ...StakeManager.unstake(farm, options) }])
     ).rejects.toThrow("407");
   });
 
-  it("fails if not called by owner of the position", async () => {
-    // Arbitrary values for testing the farm logic
-    const position: Position = {
-      fee_growth_inside_last: {
-        x: number(0),
-        y: number(0),
-      },
-      liquidity: number(150),
-      lower_tick_index: number(-10),
-      upper_tick_index: number(10),
-      owner: accounts.bob.pkh, // bob is the owner
-    };
-    const cumulativesSnapshot = {
-      tick_cumulative_inside: number(0),
-      seconds_per_liquidity_inside: Math2.bitShift(number(50), -128)
-        .dividedBy(number(100))
-        .decimalPlaces(0),
-      seconds_inside: number(0),
-    };
+  it("fails for invalid deposit", async () => {
     const incentive: Incentive = {
       reward_token: { fa12: token.address },
       start_time: NOW - 1000,
-      end_time: NOW + 1000,
+      end_time: NOW - 100, // Incentive is over in the past
       claim_deadline: NOW + 2000,
       total_reward: number(100 * DECIMALS),
       total_reward_unclaimed: number(100 * DECIMALS),
@@ -288,46 +327,51 @@ describe("farm.stake", () => {
       tick_range: { 2: -10, 3: 10 },
     });
 
-    const core = await tezos.deployContract("dummyPool", {
-      position,
-      cumulatives_inside_snapshot: cumulativesSnapshot,
-    });
-
-    storage.cfmm_address = core.address;
-
     const farm = await tezos.deployContract("farm", storage);
 
-    const options: StakeOptions = { incentiveId: 1, tokenId: 1 };
+    const options: UnstakeOptions = { incentiveId: 1, tokenId: 2 };
 
-    // When alice tries to stake for bob, txn fails
+    // When alice unstakes a position for deposit of token id 2, txn fails
     await expect(
-      tezos.sendBatchOp([{ kind: OpKind.TRANSACTION, ...StakeManager.stake(farm, options) }])
-    ).rejects.toThrow("401");
+      tezos.sendBatchOp([{ kind: OpKind.TRANSACTION, ...StakeManager.unstake(farm, options) }])
+    ).rejects.toThrow("411");
   });
 
-  it("fails if incentive is over", async () => {
-    // Arbitrary values for testing the farm logic
-    const position: Position = {
-      fee_growth_inside_last: {
-        x: number(0),
-        y: number(0),
-      },
-      liquidity: number(150),
-      lower_tick_index: number(-10),
-      upper_tick_index: number(10),
-      owner: accounts.alice.pkh,
-    };
-    const cumulativesSnapshot = {
-      tick_cumulative_inside: number(0),
-      seconds_per_liquidity_inside: Math2.bitShift(number(50), -128)
-        .dividedBy(number(100))
-        .decimalPlaces(0),
-      seconds_inside: number(0),
-    };
+  it("fails for invalid stake", async () => {
     const incentive: Incentive = {
       reward_token: { fa12: token.address },
       start_time: NOW - 1000,
-      end_time: NOW - 100,
+      end_time: NOW - 100, // Incentive is over in the past
+      claim_deadline: NOW + 2000,
+      total_reward: number(100 * DECIMALS),
+      total_reward_unclaimed: number(100 * DECIMALS),
+      total_seconds_claimed: number(0),
+      n_stakes: 1,
+      refundee: accounts.alice.pkh,
+    };
+
+    storage.incentives.set(1, incentive);
+    storage.deposits.set(1, {
+      owner: accounts.alice.pkh,
+      n_stakes: 1,
+      tick_range: { 2: -10, 3: 10 },
+    });
+
+    const farm = await tezos.deployContract("farm", storage);
+
+    const options: UnstakeOptions = { incentiveId: 1, tokenId: 1 };
+
+    // When alice unstakes a position for stake on incentive 1 with token 1, txn fails
+    await expect(
+      tezos.sendBatchOp([{ kind: OpKind.TRANSACTION, ...StakeManager.unstake(farm, options) }])
+    ).rejects.toThrow("412");
+  });
+
+  it("fails when not called by owner", async () => {
+    const incentive: Incentive = {
+      reward_token: { fa12: token.address },
+      start_time: NOW - 1000,
+      end_time: NOW - 100, // Incentive is over in the past
       claim_deadline: NOW + 2000,
       total_reward: number(100 * DECIMALS),
       total_reward_unclaimed: number(100 * DECIMALS),
@@ -347,25 +391,18 @@ describe("farm.stake", () => {
       }
     );
     storage.deposits.set(1, {
-      owner: accounts.alice.pkh,
+      owner: accounts.bob.pkh, // change owner
       n_stakes: 1,
       tick_range: { 2: -10, 3: 10 },
     });
 
-    const core = await tezos.deployContract("dummyPool", {
-      position,
-      cumulatives_inside_snapshot: cumulativesSnapshot,
-    });
-
-    storage.cfmm_address = core.address;
-
     const farm = await tezos.deployContract("farm", storage);
 
-    const options: StakeOptions = { incentiveId: 1, tokenId: 1 };
+    const options: UnstakeOptions = { incentiveId: 1, tokenId: 1 };
 
-    // When alice tries to stake on ended incentive
+    // When alice unstakes a position for bob, the txn fails
     await expect(
-      tezos.sendBatchOp([{ kind: OpKind.TRANSACTION, ...StakeManager.stake(farm, options) }])
-    ).rejects.toThrow("408");
+      tezos.sendBatchOp([{ kind: OpKind.TRANSACTION, ...StakeManager.unstake(farm, options) }])
+    ).rejects.toThrow("401");
   });
 });
